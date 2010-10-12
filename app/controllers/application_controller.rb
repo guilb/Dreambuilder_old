2ce9f406d8906ab6a39c7b0efb0457ae4bf54106
Guilb <francois.guilbert@neoweb.fr>
2010-10-12 15:35:42 +0200

    Signed-off-by: Guilb <francois.guilbert@neoweb.fr>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :meta_defaults
+  before_filter :meta_defaults, :set_iphone_format
+
+   def set_iphone_format
+     if is_iphone_request?
+       request.format = :iphone
+     end
+   end
+
+  # this method uses a regular expression to match the text
+  # that would describe the browser used by the client
+  def is_iphone_request?
+    request.user_agent =~ /(Mobile\/.+Safari)/
+    #request.user_agent =~ /(AppleWebKit\/.+AdobeAIR)/
+  end
+
   private
 
  def meta_defaults
